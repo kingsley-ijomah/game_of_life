3688963cf774164fc6274fda6a4473dfde9e0fd9
kingsley <kingsley@kocolondon.com>
2014-07-30 15:50:30 +0100

    Draw each block cell and call game tick! method within gosu update

--- game.rb
+++ game.rb
@@ -8,15 +8,21 @@ class GameOfLife < Gosu::Window
     super @width, @height, false
     self.caption = 'Game of Life'
     @background_color = Gosu::Color.new(0xffdedede)
+    @alive_color = Gosu::Color.new(0xff121212)
+    @dead_color = Gosu::Color.new(0xffededed)
     
     # Game
     @cols = width/10
     @rows = height/10
+    @cell_width = width / @cols
+    @cell_height = height / @rows
+
     @world = World.new(@cols, @rows)
     @game = Game.new(@world)
   end
 
   def update
+    @game.tick!
   end
 
   def draw
@@ -26,6 +32,22 @@ class GameOfLife < Gosu::Window
       @width, @height, @background_color,
       0, @height, @background_color
     ) 
+
+    @game.world.cells.each do |cell|
+      if cell.alive?
+        # draw a live cell
+        draw_quad(cell.x * @cell_width, cell.y * @cell_height, @alive_color,
+                  cell.x * @cell_width + @cell_width, cell.y * @cell_height, @alive_color,
+                  cell.x * @cell_width + @cell_width, cell.y * @cell_height + @cell_height, @alive_color,
+                  cell.x * @cell_width, cell.y * @cell_height + @cell_height, @alive_color) 
+      else
+        # draw a dead cell
+        draw_quad(cell.x * @cell_width, cell.y * @cell_height, @dead_color,
+                  cell.x * @cell_width + @cell_width, cell.y * @cell_height, @dead_color,
+                  cell.x * @cell_width + @cell_width, cell.y * @cell_height + @cell_height, @dead_color,
+                  cell.x * @cell_width, cell.y * @cell_height + @cell_height, @dead_color) 
+      end
+    end
   end
 
   def needs_cursor?
